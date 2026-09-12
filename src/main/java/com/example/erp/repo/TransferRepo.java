2f4b031323e9fc8ee32953a4f95a9dda7e495a11
Shubham Kumar Singh <shubhamkumarsingh200505@gmail.com>
2026-09-12 18:20:07 +0530

    fix: expose repositories as Spring Data beans

---
+++ src/main/java/com/example/erp/repo/TransferRepo.java
@@ -0,0 +1,4 @@
+package com.example.erp.repo;
+import com.example.erp.model.Models.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface TransferRepo extends JpaRepository<Transfer,Long>{}
